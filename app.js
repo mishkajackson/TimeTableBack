62bfb22791df7245743a94a3ffcb1b008076bb42
mishkajackson <92293134+mishkajackson@users.noreply.github.com>
2023-05-11 22:45:16 +0300

    43

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application.!!" });
+  res.json({ message: "Welcome to API!" });
 });
 require("./routes/users.routes.js")(app);
 
