9296608ec8a9c50f2d4f4b0d3c7838a43fba2482
mishkajackson <92293134+mishkajackson@users.noreply.github.com>
2023-05-12 08:36:57 +0300

    env

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   HOST: process.env.HOSTNAME,
-  PORT: process.env.PORT || 3306,
+  PORT: 3306,
   USER: process.env.DBUSER,
   PASSWORD: process.env.DBPASSWORD,
   DB: process.env.DB
